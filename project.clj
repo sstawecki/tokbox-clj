5bec88705dd576de31f40019c107ad002631036e
Sebastian Stawecki <sstawecki@gmail.com>
2016-01-20 12:40:02 -0300

    details

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject tokbox-clj "0.1.0-SNAPSHOT"
 
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Learning Clojure"
+  :url "https://tokbox-clj.herokuapp.com/"
 
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [selmer "1.0.0"]
